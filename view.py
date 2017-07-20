13764a25e91715d0e308e06a88b2f1be350576bd
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-07-20 10:42:10 +0200

    Add basic taboo version

---
+++ view.py
@@ -0,0 +1,38 @@
+def print_all_events(events):
+
+    for event in events:
+        print(event)
+
+def print_main_menu():
+
+    print("""
+    choose option
+    1. book private mentoring
+    2. book checkpoint
+    3. show all events""")
+
+def print_goodbye():
+
+    print("bye bye")
+
+def get_choice():
+
+    return input("choose option: ")
+
+def get_checkpoint_details():
+
+    return self.get_event_date()
+
+def get_event_date():
+
+    return input("enter the date dd-mm-yyyy: ")
+
+def get_preffered_mentor(self):
+
+    return input("enter preffered mentor: ")
+
+def get_goal(self):
+
+    return input("enter you goal: ")
+
+    
